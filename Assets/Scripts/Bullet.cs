1f9609da17a7f7bc70e0ad91387a6b91bd6d115d
OrehDM <zubyarik397@gmail.com>
2024-05-12 14:14:53 +0300

    dscfvgbt

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+    public float lifetime;
+    public float damage = 10;
+
+    private void Start()
+    {
+        Invoke("DestroyBullet", lifetime);
+    }
+
+    void FixedUpdate()
+    {
+        MoveFixedUpdate();
+    }
+    private void MoveFixedUpdate()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        DamageEnemy(collision);
+        DestroyBullet();
+    }
+    private void DestroyBullet()
+    {
+        Destroy(gameObject);
+    }
+    private void DamageEnemy(Collision collision)
+    {
+         var enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+        if (enemyHealth != null)
+        {
+            enemyHealth.DealDamage(damage);
+        }
+    }
+}
